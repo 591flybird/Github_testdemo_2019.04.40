9eafebb5e102b407357a120d0f5084e3b16219f5
manager <hjl@itcast.cn>
2019-04-20 15:33:18 +0800

    项目，推倒version1.0，重头开始

--- test.py
+++ test.py
@@ -1,9 +1,2 @@
-
-num2=2
-num5=5
-num3=3
-num4=4
-'经理本地回退版本之后，做出如下修改'
-for i in range(10):
-    'num.{}'.formart(i) +'='+'{}'.format(i)
-'提交时，落后远程仓库代码，只能先ｐｕｌｌ，再合并修改,然后在push'
+'项目失败，推到重来'
+life is short , i use python
